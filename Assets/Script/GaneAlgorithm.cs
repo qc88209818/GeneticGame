93ffe5d8571aa302c1746f246370786b4e94e77a
qianchi <915618574@qq.com>
2018-01-16 22:39:00 +0800

    + 新增工程

---
+++ Assets/Script/GaneAlgorithm.cs
@@ -0,0 +1,9 @@
+﻿/***
+ * 计算基因的算法类
+ */
+public class GaneAlgorithm
+{
+	public GaneAlgorithm ()
+	{
+	}
+}
---
+++ Assets/Script/Gene.cs
@@ -0,0 +1,10 @@
+﻿/***
+ * 基因基础类
+ */
+public class Gene {
+	private int[] _gane = new int[10];
+
+	public Gene ()
+	{
+	}
+}
